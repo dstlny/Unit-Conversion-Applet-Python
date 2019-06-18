12ea706ae153ffb0faae4f9248480d33d8a2dfb4
Luke Elam <lukeelam1999@icloud.com>
2019-06-18 18:40:31 +0100

    Got rid of some unneccesary .strip() and .replace(), replaced them with regex - which is faster. Added null check for self.contents,, some other things.

--- myApp.py
+++ myApp.py
@@ -1,5 +1,8 @@
-import wx,  os, hashlib, pathlib, glob  
+import wx
+import os
+import hashlib
 from pathlib import Path
+import re
 
 class AppFrame(wx.Frame):
 
@@ -8,13 +11,13 @@ class AppFrame(wx.Frame):
 
         # ensure the parent's __init__ is called
         super(AppFrame, self).__init__(*args, **kw)
-        self.onInitialCurrencyLoad()
         # create a menu bar
         self.makeMenuBar()
         # and a status bar
         self.CreateStatusBar()
         self.SetStatusText("Welcome to my Python port of my Java application!")
         self.SetSize(900, 750)
+        self.onInitialCurrencyLoad()
         self.mainPanel()
 
     def onInitialCurrencyLoad(self):
@@ -28,16 +31,13 @@ class AppFrame(wx.Frame):
             self.contents = []
 
             with open(self.filePath, 'r', encoding='utf-8-sig') as f:
-                self.contents = f.readlines()
+                self.contents = [re.sub(r"[\n\t]*", "", x) for x in f.readlines()]
 
-            self.contents = [x.strip('\t \n') for x in self.contents]
-            self.contents = [x.replace(', ', ',') for x in self.contents]
-
-            for i in self.contents:
-                currName, currFactor, currSymbol = i.split(',')
-                self.currencyCombo.append('British Pounds (GBP) to ' + currName)
-                self.currencyFactors.append(float(currFactor))
-                self.currencySymbols.append(currSymbol)
+                for i in self.contents:
+                    currName, currFactor, currSymbol = i.split(',')
+                    self.currencyCombo.append('British Pounds (GBP) to ' + currName)
+                    self.currencyFactors.append(float(currFactor))
+                    self.currencySymbols.append(currSymbol)
 
         except Exception as exception:
             print(exception)
@@ -103,7 +103,7 @@ class AppFrame(wx.Frame):
 
         # Creating various UI elements for the unit file-selection module
         self.fileDirectorySelection = wx.StaticBox(self.mainPanel, wx.ID_ANY, "File/Directory Selection")
-        self.fileDirectoryButton = wx.Button(self.mainPanel, wx.ID_ANY, "Click to select a file/directory to inspect", size=(300, 30))
+        self.fileDirectoryButton = wx.Button(self.mainPanel,1003, "Click to select a file/directory to inspect", size=(300, 30))
         self.choices = ['Inspect single file (default)', 'Inspect directory', 'Inspect directory meta-data']
         self.fileSelectionChoices = wx.RadioBox(self.mainPanel, wx.ID_ANY, choices=self.choices, style=wx.RA_SPECIFY_ROWS)
         self.writeToFile = wx.CheckBox(self.mainPanel, wx.ID_ANY, label='Output generated output to disk', style=wx.RA_SPECIFY_ROWS)
@@ -127,7 +127,7 @@ class AppFrame(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.onCurrConvert, self.currConvertButton)
         self.Bind(wx.EVT_BUTTON, self.onUnitConvert, self.unitConvertButton)
         self.Bind(wx.EVT_BUTTON, self.onClear, self.clearThings)
-        self.Bind(wx.EVT_BUTTON, self.loadFileorDirectory, self.fileDirectoryButton)
+        self.Bind(wx.EVT_BUTTON, self.loadFileorDirectory, self.fileDirectoryButton, id=1003)
 
         # Basically making sure it's responsive
         self.outHSizer.Add(self.sizer, flag=wx.ALL, proportion=15)
@@ -140,26 +140,72 @@ class AppFrame(wx.Frame):
 
         fileMenu = wx.Menu()
         exitItem = fileMenu.Append(wx.ID_EXIT)
-        loadCurrency = fileMenu.Append(wx.ID_OPEN, '&Load Currency File..')
-        loadFileOrDirectory = fileMenu.Append(wx.ID_OPEN, '&Load File or Directory...')
+        loadCurrency = fileMenu.Append(1001, '&Load Currency File..')
+        loadFileOrDirectory = fileMenu.Append(1002, '&Load File or Directory...')
         helpMenu = wx.Menu()
         aboutItem = helpMenu.Append(wx.ID_ABOUT)
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu, "&File")
         menuBar.Append(helpMenu, "&Help")
         self.SetMenuBar(menuBar)
-        self.Bind(wx.EVT_MENU, self.onExit,  exitItem)
-        self.Bind(wx.EVT_MENU, self.onAbout, aboutItem)
-        self.Bind(wx.EVT_MENU, self.onLoadCurrency, loadCurrency)
-        self.Bind(wx.EVT_MENU, self.loadFileorDirectory, loadFileOrDirectory)
+        self.Bind(wx.EVT_MENU, self.onExit,source=exitItem)
+        self.Bind(wx.EVT_MENU, self.onAbout,source=aboutItem)
+        self.Bind(wx.EVT_MENU, self.onLoadCurrency,source=loadCurrency, id=1001)
+        self.Bind(wx.EVT_MENU, self.loadFileorDirectory,source=loadFileOrDirectory,id=1002)
 
     def onExit(self, event):
         """Close the frame, terminating the application."""
         self.Close(True)
 
+    def onLoadCurrency(self, event):
+        self.defaultPath = os.path.dirname(os.path.realpath(__file__))
+        self.openFileDialog = wx.FileDialog(self, "Open and import a currency...", self.defaultPath , "","Text files (*.txt)|*.txt", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+        self.openFileDialog.ShowModal()
+
+        try:
+            self.currencyCombo = []
+            self.currencyFactors = []
+            self.currencySymbols = []
+            self.contents = []
+            self.currName = ""
+            self.currFactor = ""
+            self.currSymbol = ""
+
+            with open(self.openFileDialog.GetPath(), 'r', encoding='utf-8-sig') as f:
+                self.contents = [re.sub(r"[\n\t]*", "", x) for x in f.readlines()]
+
+                for i in self.contents:
+
+                    try:
+
+                        self.currName, self.currFactor, self.currSymbol = i.split(',')
+
+                        try:
+                            self.currFactor = float(self.currFactor)
+                        except Exception as exception:
+                            self.currName = ""
+                            self.currFactor = 00.00
+                            self.currSymbol = ""
+                            continue
+
+                    except ValueError as exception:
+                        continue
+
+                    finally:
+                        self.currencyCombo.append('British Pounds (GBP) to ' + self.currName)
+                        self.currencyFactors.append(self.currFactor)
+                        self.currencySymbols.append(self.currSymbol)
+                        self.currencyDrop.Clear()
+                        self.currencyDrop.AppendItems(self.currencyCombo)
+                        self.measurementDrop.SetSelection(0)
+                        self.currencyDrop.SetSelection(0)
+
+        except Exception as exception:
+            print(exception)
+    
     def loadFileorDirectory(self, event):
         self.defaultPath = os.path.dirname(os.path.realpath(__file__))
-        self.openFileDialog = wx.FileDialog(self, "Open a currency file...", self.defaultPath, "","", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+        self.openFileDialog = wx.FileDialog(self, "Open and load a directory...", self.defaultPath, "","", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
         self.openFileDialog.ShowModal()
 
         try:
@@ -173,57 +219,6 @@ class AppFrame(wx.Frame):
 
         except Exception as excepti:
             print(excepti)
-
-    def onLoadCurrency(self, event):
-        self.defaultPath = os.path.dirname(os.path.realpath(__file__))
-        self.openFileDialog = wx.FileDialog(self, "Open and scan a file...", self.defaultPath , "","Text files (*.txt)|*.txt", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
-        self.openFileDialog.ShowModal()
-
-        try:
-            self.currencyCombo.clear()
-            self.currencyFactors.clear()
-            self.currencySymbols.clear()
-            self.contents.clear()
-            self.currName = ""
-            self.currFactor = ""
-            self.currSymbol = ""
-
-            with open(self.openFileDialog.GetPath(), 'r', encoding='utf-8-sig') as f:
-                self.contents = f.readlines()
-
-            self.contents = [x.strip('\t \n') for x in self.contents]
-            self.contents = [x.replace('\t', '') for x in self.contents]
-            self.contents = [x.replace(', ', ',') for x in self.contents]
-
-            for i in self.contents:
-
-                try:
-
-                    self.currName, self.currFactor, self.currSymbol = i.split(',')
-
-                    try:
-                        self.currFactor = float(self.currFactor)
-                    except Exception as exception:
-
-                        self.currName = ""
-                        self.currFactor = 00.00
-                        self.currSymbol = ""
-                        continue
-
-                except ValueError as exception:
-                    continue
-
-                else:
-                    self.currencyCombo.append('British Pounds (GBP) to ' + self.currName)
-                    self.currencyFactors.append(self.currFactor)
-                    self.currencySymbols.append(self.currSymbol)
-                    self.currencyDrop.Clear()
-                    self.currencyDrop.AppendItems(self.currencyCombo)
-                    self.measurementDrop.SetSelection(0)
-                    self.currencyDrop.SetSelection(0)
-
-        except Exception as exception:
-            print(exception)
 
     def onClear(self, event):
         """Do something"""
@@ -357,8 +352,8 @@ class AppFrame(wx.Frame):
 
         try:
             with open(directory+'\\'+filename, 'r') as f:
-                self.contents = f.readlines()
-                self.contents = [x.strip('\t \n') for x in self.contents]
+                self.contents = [re.sub(r"[\n\t]*", "", x) for x in f.readlines()]
+
         except Exception as ex:
             print(ex)
         else:
@@ -404,9 +399,10 @@ class AppFrame(wx.Frame):
         for item in files_in_basepath:
             try:
                 with open(directory+"\\"+item.name, 'r') as f:
-                    self.contents = f.readlines()
-                    self.contents = [x.strip('\t \n') for x in self.contents]
+                    self.contents = [re.sub(r"[\n\t]*", "", x) for x in f.readlines()]
+
             except Exception as exceptio:
+                print(exceptio)
                 continue
             finally:
 
@@ -416,6 +412,7 @@ class AppFrame(wx.Frame):
                     if self.writeToFile.GetValue() == True:
                         stringToWrite = directory+' : '+item.name+' : '+str(self.returnUserChoice())+' : '+str(self.returnAlgorithm())+' : '+str(self.algo.md5.getHash(self)[0:128])
                         self.writeLineToFile(stringToWrite, str(self.algo.md5.getHash(self)[0:128]))
+                        print(stringToWrite)
                     else:
                         stringToWrite = directory+' : '+item.name+' : '+str(self.returnUserChoice())+' : '+str(self.returnAlgorithm())+' : '+str(self.algo.md5.getHash(self)[0:128])
                         print(stringToWrite)
@@ -490,39 +487,46 @@ class AppFrame(wx.Frame):
                 f.write(lineToWrite+'\n')
                 f.close()
         else:
-            self.contents = set([line.strip("\n") for line in open(self.defaultPath+"final.txt")])
+            self.contents = set([re.sub(r"[\n\t]*", "", line) for line in open(self.defaultPath+"final.txt")])
             
             imported_path, imported_file, imported_choice, imported_algo, imported_hashy_boi = lineToWrite.split(" : ")
             imported_hashy_boi = self.hash
 
-            for line in self.contents:
-                path_name, file_name, user_choice, algorithm_choice, hashy_boi = line.split(" : ")
+            if not self.contents:
+                print('{} written'.format(lineToWrite))
+                with open(self.defaultPath+"final.txt","w+") as f:
+                    f.write(lineToWrite+'\n')
+                    f.close()
+            else:
 
-                if imported_path == path_name and imported_file == file_name and imported_choice == user_choice and imported_algo == algorithm_choice and imported_hashy_boi != hashy_boi:
-                    this_string = path_name + ' : ' + file_name  + ' : ' + user_choice  + ' : ' + algorithm_choice + ' : ' + hashy_boi
-                    string_to_add = imported_path  + ' : ' + imported_file  + ' : ' + imported_choice  + ' : ' + imported_algo + ' : ' + imported_hashy_boi
-                    self.contents.remove(this_string)
-                    self.contents.add(string_to_add)
-                    self.onAltered(file_name, hashy_boi, imported_hashy_boi)
-                    print('-------------\nfallen into main if statement\nline removed = {}\nline added = {}'.format(this_string, string_to_add))
-                    for extra in self.contents:
-                        print('{} is unique and in self.contents'.format(extra))
-                    self.cleaned = True
+                for line in self.contents:
+                    path_name, file_name, user_choice, algorithm_choice, hashy_boi = line.split(" : ")
 
-                    if self.cleaned:
-                        with open(self.defaultPath+"final.txt","w+") as f:
-                            for self.this_line in self.contents:
-                                f.write(self.this_line+'\n')
-                
-                else: ##this must mean that the curent imported file does not exist within final.txt, thus print it to the file
-                    string_to_add = imported_path  + ' : ' + imported_file  + ' : ' + imported_choice  + ' : ' + imported_algo + ' : ' + imported_hashy_boi
-                    self.contents = set([line.strip("\n") for line in open(self.defaultPath+"final.txt")])
-                    self.contents.add(string_to_add)
-                    print('-------------\nfallen into else')
-                    with open(self.defaultPath+"final.txt","w+") as f:
+                    if imported_path == path_name and imported_file == file_name and imported_choice == user_choice and imported_algo == algorithm_choice and imported_hashy_boi != hashy_boi:
+                        this_string = path_name + ' : ' + file_name  + ' : ' + user_choice  + ' : ' + algorithm_choice + ' : ' + hashy_boi
+                        string_to_add = imported_path  + ' : ' + imported_file  + ' : ' + imported_choice  + ' : ' + imported_algo + ' : ' + imported_hashy_boi
+                        self.contents.remove(this_string)
+                        self.contents.add(string_to_add)
+                        self.onAltered(file_name, hashy_boi, imported_hashy_boi)
+                        print('-------------\nfallen into main if statement\nline removed = {}\nline added = {}'.format(this_string, string_to_add))
                         for extra in self.contents:
                             print('{} is unique and in self.contents'.format(extra))
-                            f.write(extra+'\n')
+                        self.cleaned = True
+
+                        if self.cleaned:
+                            with open(self.defaultPath+"final.txt","w+") as f:
+                                for self.this_line in self.contents:
+                                    f.write(self.this_line+'\n')
+                    
+                    else: ##this must mean that the curent imported file does not exist within final.txt, thus print it to the file
+                        string_to_add = imported_path  + ' : ' + imported_file  + ' : ' + imported_choice  + ' : ' + imported_algo + ' : ' + imported_hashy_boi
+                        self.contents = set([line.strip("\n") for line in open(self.defaultPath+"final.txt")])
+                        self.contents.add(string_to_add)
+                        print('-------------\nfallen into else')
+                        with open(self.defaultPath+"final.txt","w+") as f:
+                            for extra in self.contents:
+                                print('{} is unique and in self.contents'.format(extra))
+                                f.write(extra+'\n')
 
     def onAltered(self, fileAltered, oldFileHash, newFileHash):
         """Display dialog saying it's empty"""
